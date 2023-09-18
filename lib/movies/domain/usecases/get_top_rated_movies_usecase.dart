73b1766d35f61c4679bb5cbca34d90fa81bd3cae
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-18 15:42:40 +0200

    final BaseMoviesRepository baseMoviesRepository;

--- lib/movies/domain/usecases/get_top_rated_movies_usecase.dart
+++ lib/movies/domain/usecases/get_top_rated_movies_usecase.dart
@@ -1,1 +1,7 @@
-class GetTopRatedMoviesUseCase {}
+import 'package:movies_appcleanarchitecture_elgendy_v1/movies/domain/repository/base_movies_repository.dart';
+
+class GetTopRatedMoviesUseCase {
+  final BaseMoviesRepository baseMoviesRepository;
+
+  GetTopRatedMoviesUseCase(this.baseMoviesRepository);
+}
