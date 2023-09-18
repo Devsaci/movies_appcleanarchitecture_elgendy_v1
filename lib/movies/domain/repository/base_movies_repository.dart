e4fd3eb66deffcb70f70c3e1e07413b7b3dea442
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-18 15:20:53 +0200

    Future<List<Movie>> getTopRatedMovies();

--- lib/movies/domain/repository/base_movies_repository.dart
+++ lib/movies/domain/repository/base_movies_repository.dart
@@ -3,4 +3,5 @@ import '../entiies/movie.dart';
 abstract class BaseMoviesRepository {
   Future<List<Movie>> getNowPlayingMovies();
   Future<List<Movie>> getPopularMovies();
+  Future<List<Movie>> getTopRatedMovies();
 }
