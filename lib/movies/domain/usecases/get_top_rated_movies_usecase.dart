f76240fff3840ec306e0057f8316a0e9c6c8f971
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-18 15:45:41 +0200

    return await baseMoviesRepository.getTopRatedMovies();

--- lib/movies/domain/usecases/get_top_rated_movies_usecase.dart
+++ lib/movies/domain/usecases/get_top_rated_movies_usecase.dart
@@ -1,9 +1,13 @@
 import 'package:movies_appcleanarchitecture_elgendy_v1/movies/domain/repository/base_movies_repository.dart';
 
+import '../entiies/movie.dart';
+
 class GetTopRatedMoviesUseCase {
   final BaseMoviesRepository baseMoviesRepository;
 
   GetTopRatedMoviesUseCase(this.baseMoviesRepository);
 
-  excute() {}
+  Future<List<Movie>> excute() async {
+    return await baseMoviesRepository.getTopRatedMovies();
+  }
 }
