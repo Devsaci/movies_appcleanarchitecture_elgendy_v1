968f8e8c04352593958ab36167c89903fc95536b
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-19 12:57:26 +0200

    return [];

--- lib/movies/data/datasource/movie_remote_data_source.dart
+++ lib/movies/data/datasource/movie_remote_data_source.dart
@@ -6,9 +6,9 @@ class MovieRemoteDataSource {
     final response = await Dio().get(
         "https://api.themoviedb.org/3/movie/now_playing?api_key=779f107914e78668bbcc2adc00e6c286&language=en-US&page=1");
     if (response.statusCode == 200) {
-      return null;
+      return [];
     } else {
-      return null;
+      return [];
     }
   }
 }
