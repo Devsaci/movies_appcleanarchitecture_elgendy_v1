7b1cd682b2e781b5aef665e4dcfb8497a0738336
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-19 10:17:41 +0200

    getNowPlayingMovies() {

--- lib/movies/data/datasource/movie_remote_data_source.dart
+++ lib/movies/data/datasource/movie_remote_data_source.dart
@@ -1,1 +1,5 @@
-class MovieRemoteDataSource {}
+import 'package:movies_appcleanarchitecture_elgendy_v1/movies/data/entities/movie.dart';
+
+class MovieRemoteDataSource {
+  getNowPlayingMovies() {}
+}
