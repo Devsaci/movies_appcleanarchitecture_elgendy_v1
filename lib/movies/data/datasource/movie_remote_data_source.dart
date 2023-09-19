1a56224d0c344465a25276cd144de3f33e99ac91
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-19 12:57:55 +0200

    return [];

--- lib/movies/data/datasource/movie_remote_data_source.dart
+++ lib/movies/data/datasource/movie_remote_data_source.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:movies_appcleanarchitecture_elgendy_v1/movies/data/entities/movie.dart';
 
 class MovieRemoteDataSource {
-  Future<List<MovieModel>?> getNowPlayingMovies() async {
+  Future<List<MovieModel>> getNowPlayingMovies() async {
     final response = await Dio().get(
         "https://api.themoviedb.org/3/movie/now_playing?api_key=779f107914e78668bbcc2adc00e6c286&language=en-US&page=1");
     if (response.statusCode == 200) {
