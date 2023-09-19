47a26cf4089978cbe575dcb36b29e010c5143023
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-19 09:23:11 +0200

    class MovieModel extends Movie

--- lib/movies/data/entities/movie.dart
+++ lib/movies/data/entities/movie.dart
@@ -1,1 +1,13 @@
-class MovieModel {}
+import 'package:movies_appcleanarchitecture_elgendy_v1/movies/domain/entiies/movie.dart';
+
+class MovieModel extends Movie {
+  const MovieModel({
+    required super.id,
+    required super.title,
+    required super.backdropPath,
+    required super.overview,
+    required super.voteAverage,
+    required super.genreIds,
+    required super.releaseDate,
+  });
+}
