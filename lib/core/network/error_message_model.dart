1dd25116b224d896a2b16021f0ea85ad899adb6b
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-20 09:51:11 +0200

    late final int statusCode;

--- lib/core/network/error_message_model.dart
+++ lib/core/network/error_message_model.dart
@@ -1,6 +1,7 @@
 import 'package:equatable/equatable.dart';
 
 class ErrorMessageModel extends Equatable {
+  late final int statusCode;
   @override
   // TODO: implement props
   List<Object?> get props => throw UnimplementedError();
