a47121afc2c803bee03ab87440eba7e80804ddce
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-20 07:55:37 +0200

    return List<MovieModel>.from(response.data['results']);

--- lib/movies/data/datasource/movie_remote_data_source.dart
+++ lib/movies/data/datasource/movie_remote_data_source.dart
@@ -6,7 +6,7 @@ class MovieRemoteDataSource {
     final response = await Dio().get(
         "https://api.themoviedb.org/3/movie/now_playing?api_key=779f107914e78668bbcc2adc00e6c286&language=en-US&page=1");
     if (response.statusCode == 200) {
-      return [];
+      return List<MovieModel>.from(response.data['results']);
     } else {
       return [];
     }
