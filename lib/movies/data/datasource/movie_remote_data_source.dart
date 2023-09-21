c27c6310691b6689873c1660a37a03230dba3f0c
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-09-21 15:13:02 +0200

    throw ServerExceptio

--- lib/movies/data/datasource/movie_remote_data_source.dart
+++ lib/movies/data/datasource/movie_remote_data_source.dart
@@ -4,12 +4,16 @@ import 'package:movies_appcleanarchitecture_elgendy_v1/core/network/error_messag
 import 'package:movies_appcleanarchitecture_elgendy_v1/movies/data/entities/movie.dart';
 
 class MovieRemoteDataSource {
-  Future<List<MovieModel>?> getNowPlayingMovies() async {
+  Future<List<MovieModel>> getNowPlayingMovies() async {
     final response = await Dio().get(
         "https://api.themoviedb.org/3/movie/now_playing?api_key=779f107914e78668bbcc2adc00e6c286&language=en-US&page=1");
     if (response.statusCode == 200) {
       return List<MovieModel>.from((response.data['results'] as List)
           .map((e) => MovieModel.fromJson(e)));
-    } else {}
+    } else {
+      throw ServerException(
+        errorMessageModel: ErrorMessageModel.fromJson(response.data),
+      );
+    }
   }
 }
